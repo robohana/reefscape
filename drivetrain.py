be4fe1d5d79fab1f1be99f3edaa51c609675ad0a
Jesse Lard <jesselard@gmail.com>
2025-01-18 11:12:43 -0600

    Update drivetrain.py

--- drivetrain.py
+++ drivetrain.py
@@ -1,22 +1,15 @@
-
-
 import math
 import wpilib
 import wpimath.geometry
 import wpimath.kinematics
 import swervemodule
-from wpilib import SmartDashboard as sd
-
-
+from navx import AHRS  # Import the AHRS class from the navX library
 
 kMaxSpeed = 3.0  # 3 meters per second
 kMaxAngularSpeed = math.pi  # 1/2 rotation per second
 
-
 class Drivetrain:
-    """
-    Represents a swerve drive style drivetrain.
-    """
+    """Represents a swerve drive style drivetrain."""
 
     def __init__(self) -> None:
         self.frontLeftLocation = wpimath.geometry.Translation2d(0.381, 0.381)
@@ -29,7 +22,7 @@ class Drivetrain:
         self.backLeft = swervemodule.SwerveModule(24, 30, 2)
         self.backRight = swervemodule.SwerveModule(21, 29, 1)
 
-        self.gyro =  wpilib.ADXRS450_Gyro()
+        self.gyro = AHRS(wpilib.SPI.Port.kMXP)  # Initialize the navX gyro on the MXP port
 
         self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
             self.frontLeftLocation,
@@ -40,7 +33,7 @@ class Drivetrain:
 
         self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
             self.kinematics,
-            self.gyro.getRotation2d(),
+            wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle()),  # Get the angle from the navX gyro
             (
                 self.frontLeft.getPosition(),
                 self.frontRight.getPosition(),
@@ -49,8 +42,7 @@ class Drivetrain:
             ),
         )
 
-        self.gyro.reset()
-
+        self.gyro.reset()  # Reset the navX gyro
 
     def drive(
         self,
@@ -60,46 +52,83 @@ class Drivetrain:
         fieldRelative: bool,
         periodSeconds: float,
     ) -> None:
-        """
-        Method to drive the robot using joystick info.
+        """Method to drive the robot using joystick info.
+
         :param xSpeed: Speed of the robot in the x direction (forward).
         :param ySpeed: Speed of the robot in the y direction (sideways).
         :param rot: Angular rate of the robot.
         :param fieldRelative: Whether the provided x and y speeds are relative to the field.
         :param periodSeconds: Time
         """
-        swerveModuleStates = self.kinematics.toSwerveModuleStates(
-            wpimath.kinematics.ChassisSpeeds.discretize(
-                (
-                    wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
-                        xSpeed, ySpeed, rot, self.gyro.getRotation2d()
-                    )
-                    if fieldRelative
-                    else wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot)
-                ),
-                periodSeconds,
+        if fieldRelative:
+            # Transform the desired motion vector from robot-relative to field-relative
+            chassisSpeeds = wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
+                xSpeed, ySpeed, rot, wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle())
             )
+        else:
+            chassisSpeeds = wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot)
+
+        swerveModuleStates = self.kinematics.toSwerveModuleStates(
+            wpimath.kinematics.ChassisSpeeds.discretize(chassisSpeeds, periodSeconds)
         )
+        
         wpimath.kinematics.SwerveDrive4Kinematics.desaturateWheelSpeeds(
             swerveModuleStates, kMaxSpeed
         )
+        
         self.frontLeft.setDesiredState(swerveModuleStates[0])
         self.frontRight.setDesiredState(swerveModuleStates[1])
         self.backLeft.setDesiredState(swerveModuleStates[2])
         self.backRight.setDesiredState(swerveModuleStates[3])
 
+        self.logEncoderValues()
+
+
+    def logEncoderValues(self) -> None:
+        """Logs the encoder values for each swerve module."""
+        #wpilib.SmartDashboard.putNumber("Front Left Drive Encoder", round(self.frontLeft.driveEncoder.getPosition(), 4))
+        wpilib.SmartDashboard.putNumber("Front Left Turning Encoder", self.frontLeft.turningEncoder.getDistance()*10000)
+
+
+        wpilib.SmartDashboard.putNumber("Front Left Turning Encoder num of turns", self.frontLeft.turningEncoder.get())
+
+        #wpilib.SmartDashboard.putNumber("Front Right Drive Encoder", round(self.frontRight.driveEncoder.getPosition(), 4))
+        wpilib.SmartDashboard.putNumber("Front Right Turning Encoder", self.frontRight.turningEncoder.getDistance()*10000)
+
+        wpilib.SmartDashboard.putNumber("Front Right Turning Encoder num of turns", self.frontRight.turningEncoder.get())
+
+        #wpilib.SmartDashboard.putNumber("Back Left Drive Encoder", round(self.backLeft.driveEncoder.getPosition(), 4))
+        wpilib.SmartDashboard.putNumber("Back Left Turning Encoder", self.backLeft.turningEncoder.getDistance()*10000)
+
+        wpilib.SmartDashboard.putNumber("Back Left Turning Encoder num of turns", self.backLeft.turningEncoder.get())
+
+        #wpilib.SmartDashboard.putNumber("Back Right Drive Encoder", round(self.backRight.driveEncoder.getPosition(), 4))
+        wpilib.SmartDashboard.putNumber("Back Right Turning Encoder", self.backRight.turningEncoder.getDistance()*10000)
+
+        wpilib.SmartDashboard.putNumber("Back Right Turning Encoder num of turns", self.backRight.turningEncoder.get())
+
     def updateOdometry(self) -> None:
         """Updates the field relative position of the robot."""
         self.odometry.update(
-            self.gyro.getRotation2d(),
+            wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle()),
             (
                 self.frontLeft.getPosition(),
                 self.frontRight.getPosition(),
                 self.backLeft.getPosition(),
                 self.backRight.getPosition(),
             ),
-
         )
 
 
 
+    def rotateWheels(self, angle: float) -> None:
+        """Rotates all swerve modules to the specified angle."""
+        desiredState = wpimath.kinematics.SwerveModuleState(0, wpimath.geometry.Rotation2d.fromDegrees(angle))
+        self.frontLeft.setDesiredState(desiredState)
+        self.frontRight.setDesiredState(desiredState)
+        self.backLeft.setDesiredState(desiredState)
+        self.backRight.setDesiredState(desiredState)
+
+    def autonomousRoutine(self) -> None:
+        """Simple autonomous routine that rotates wheels 90 degrees to the right and then back to 0."""
+        self.rotateWheels(90)
