b73a0dd5dcda5b4501cf8fdfed4452b4ac831614
Liliana <lilianacarlson.lrc@gmail.com>
2025-01-31 13:47:16 -0600

    Update drivetrain.py

--- drivetrain.py
+++ drivetrain.py
@@ -3,13 +3,16 @@ import wpilib
 import wpimath.geometry
 import wpimath.kinematics
 import swervemodule
-from navx import AHRS  # Import the AHRS class from the navX library
+import rev
+from wpilib import DataLogManager, DriverStation
+ 
+#from navx import AHRS  # Import the AHRS class from the navX library
 
 kMaxSpeed = 3.0  # 3 meters per second
 kMaxAngularSpeed = math.pi  # 1/2 rotation per second
 
 class Drivetrain:
-    """Represents a swerve drive style drivetrain."""
+    """Represents a swerve drive style drivetrain. - JL"""
 
     def __init__(self) -> None:
         self.frontLeftLocation = wpimath.geometry.Translation2d(0.381, 0.381)
@@ -17,12 +20,17 @@ class Drivetrain:
         self.backLeftLocation = wpimath.geometry.Translation2d(-0.381, 0.381)
         self.backRightLocation = wpimath.geometry.Translation2d(-0.381, -0.381)
 
-        self.frontLeft = swervemodule.SwerveModule(20, 31, 3)
-        self.frontRight = swervemodule.SwerveModule(23, 22, 0)
-        self.backLeft = swervemodule.SwerveModule(24, 30, 2)
-        self.backRight = swervemodule.SwerveModule(21, 29, 1)
+        self.frontLeft = swervemodule.SwerveModule(20, 31, 3, invertDrive = False, invertTurn = False)
+        self.frontRight = swervemodule.SwerveModule(23, 22, 0, invertDrive = True, invertTurn = True)
+        self.backLeft = swervemodule.SwerveModule(24, 30, 2, invertDrive = False, invertTurn = False)
+        self.backRight = swervemodule.SwerveModule(21, 29, 1, invertDrive = True, invertTurn = False)
 
-        self.gyro = AHRS(wpilib.SPI.Port.kMXP)  # Initialize the navX gyro on the MXP port
+        self.gyro = wpilib.ADXRS450_Gyro()
+       
+        self.gyro.reset()
+        
+        # FR_isInverted = self.frontRight.driveMotor.getInverted()
+        # wpilib.SmartDashboard.putBoolean(f"Front Right Inverted?", FR_isInverted)
 
         self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
             self.frontLeftLocation,
@@ -33,7 +41,7 @@ class Drivetrain:
 
         self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
             self.kinematics,
-            wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle()),  # Get the angle from the navX gyro
+            self.gyro.getRotation2d(),
             (
                 self.frontLeft.getPosition(),
                 self.frontRight.getPosition(),
@@ -42,17 +50,21 @@ class Drivetrain:
             ),
         )
 
-        self.gyro.reset()  # Reset the navX gyro
+        # self.timer = wpilib.Timer()
+        # self.timer.start()
+
+        # wpilib.DataLogManager.start()
+        # self.log = wpilib.DataLogManager.getLog()
 
     def drive(
         self,
         xSpeed: float,
         ySpeed: float,
         rot: float,
-        fieldRelative: bool,  # Ensure field-relative mode is enabled by default
+        fieldRelative: bool,  # Ensure field-relative mode is enabled by default - JL
         periodSeconds: float,
     ) -> None:
-        """Method to drive the robot using joystick info.
+        """Method to drive the robot using joystick info. - JL
 
         :param xSpeed: Speed of the robot in the x direction (forward).
         :param ySpeed: Speed of the robot in the y direction (sideways).
@@ -61,9 +73,9 @@ class Drivetrain:
         :param periodSeconds: Time
         """
         if fieldRelative:
-            # Transform the desired motion vector from robot-relative to field-relative
+            # Transform the desired motion vector from robot-relative to field-relative - JL
             chassisSpeeds = wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(
-                xSpeed, ySpeed, rot, wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle())
+                xSpeed, ySpeed, rot, self.gyro.getRotation2d()
             )
         else:
             chassisSpeeds = wpimath.kinematics.ChassisSpeeds(xSpeed, ySpeed, rot)
@@ -81,36 +93,53 @@ class Drivetrain:
         self.backLeft.setDesiredState(swerveModuleStates[2])
         self.backRight.setDesiredState(swerveModuleStates[3])
 
-        self.logEncoderValues()
+        self.resetEncoders()
 
-    def logEncoderValues(self) -> None:
-        """Logs the encoder values for each swerve module."""
-        # Log encoder values for debugging
-        # wpilib.SmartDashboard.putNumber("Front Left Turning Encoder", self.frontLeft.turningEncoder.getDistance()*10000)
-        # wpilib.SmartDashboard.putNumber("Front Left Turning Encoder num of turns", self.frontLeft.turningEncoder.get())
-        # wpilib.SmartDashboard.putNumber("Front Right Turning Encoder", self.frontRight.turningEncoder.getDistance()*10000)
-        # wpilib.SmartDashboard.putNumber("Front Right Turning Encoder num of turns", self.frontRight.turningEncoder.get())
-        # wpilib.SmartDashboard.putNumber("Back Left Turning Encoder", self.backLeft.turningEncoder.getDistance()*10000)
-        # wpilib.SmartDashboard.putNumber("Back Left Turning Encoder num of turns", self.backLeft.turningEncoder.get())
-        # wpilib.SmartDashboard.putNumber("Back Right Turning Encoder", self.backRight.turningEncoder.getDistance()*10000)
-        # wpilib.SmartDashboard.putNumber("Back Right Turning Encoder num of turns", self.backRight.turningEncoder.get())
+        # Telemetry - LC 1/30/25
+        # self.showInverted()
+        #self.dataManager()
+
+    def showInverted(self) -> None:
+        # FR_isInverted = self.frontRight.driveMotor.getInverted() 
+        # FL_isInverted = self.frontLeft.driveMotor.getInverted() 
+        # BR_isInverted = self.backRight.driveMotor.getInverted() 
+        # BL_isInverted = self.backLeft.driveMotor.getInverted()
+
+        # Check if half of a second has passed - LC 1/30/25
+        # Test 1, add TIMER to control data flow - LC 1/30/25
+        # if self.timer.hasElapsed(0.5):
+            # Reset the timer to count another 5 seconds, this is how you get it to loop every 5 seconds - LC 1/30/25
+            # Removed right now because i only want to know at the start of the op which motors are inverted - LC 1/30/25
+            #self.timer.reset()           
+            """Logs the motor invertion. Shows as a boolean value or green or red. - LC 1/30/25"""
+            # Log if motor is inverted  for debugging
+            #wpilib.SmartDashboard.putBoolean(f"Front Left Inverted?", FL_isInverted)
+            #wpilib.SmartDashboard.putBoolean(f"Back Right Inverted?", BR_isInverted)
+            #wpilib.SmartDashboard.putBoolean(f"Back Left Inverted?", BL_isInverted)
+        
+
+        # Test 3, see if this can use the DATALOGMANAGER to get out data instead of printing and causing the RIO CPU to spike
+        # produced a very weird repeating "Front Left Inverted Data Log Manager: False" in my terminal after I would test the 
+        # code - LC 1/30/25
+        #wpilib.DataLogManager.log(f"Front Left Inverted Data Log Manager: {FL_isInverted}")
+
 
     def resetEncoders(self) -> None:
-        """Resets the encoders for all swerve modules."""
+        """Resets the encoders for all swerve modules. - JL"""
         self.frontLeft.driveEncoder.setPosition(0)
         self.frontRight.driveEncoder.setPosition(0)
         self.backLeft.driveEncoder.setPosition(0)
         self.backRight.driveEncoder.setPosition(0)
         
-        self.frontLeft.turningEncoder.reset()
+        '''self.frontLeft.turningEncoder.reset()
         self.frontRight.turningEncoder.reset()
         self.backLeft.turningEncoder.reset()
-        self.backRight.turningEncoder.reset()
+        self.backRight.turningEncoder.reset()'''
 
     def updateOdometry(self) -> None:
-        """Updates the field relative position of the robot."""
+        """Updates the field relative position of the robot. - JL"""
         self.odometry.update(
-            wpimath.geometry.Rotation2d.fromDegrees(self.gyro.getAngle()),
+            self.gyro.getRotation2d(),
             (
                 self.frontLeft.getPosition(),
                 self.frontRight.getPosition(),
@@ -120,7 +149,7 @@ class Drivetrain:
         )
 
     def rotateWheels(self, angle: float) -> None:
-        """Rotates all swerve modules to the specified angle."""
+        """Rotates all swerve modules to the specified angle. - JL"""
         desiredState = wpimath.kinematics.SwerveModuleState(0, wpimath.geometry.Rotation2d.fromDegrees(angle))
         self.frontLeft.setDesiredState(desiredState)
         self.frontRight.setDesiredState(desiredState)
@@ -128,5 +157,11 @@ class Drivetrain:
         self.backRight.setDesiredState(desiredState)
 
     def autonomousRoutine(self) -> None:
-        """Simple autonomous routine that rotates wheels 90 degrees to the right and then back to 0."""
+        """Simple autonomous routine that rotates wheels 90 degrees to the right and then back to 0. - JL"""
         self.rotateWheels(90)
+
+    # def dataManager(self) -> None :
+    #     """This appends the data into the internal log, which is stored on the RoboRIO. This is all of the DS controls and 
+    #     joystick data - LC 1/30/25"""
+    #     # Record both DS control and joystick data - LC 1/30/25
+    #     DriverStation.startDataLog(DataLogManager.getLog())
