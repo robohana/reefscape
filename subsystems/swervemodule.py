2cada7714898db678efe1b21aa44a717ab6c9905
Jesse Lard <jesselard@gmail.com>
2025-02-14 17:08:20 -0600

    Update swervemodule.py

--- subsystems/swervemodule.py
+++ subsystems/swervemodule.py
@@ -1,10 +1,3 @@
-# Type: Swerve Module
-''' 
-This file contains the SwerveModule class, which represents a single swerve module on the robot.
-
-'''
-
-
 import math
 import wpilib
 import time
@@ -18,8 +11,7 @@ from wpimath.trajectory import TrajectoryConfig
 from wpilib import AnalogEncoder
 import commands2
 
-from wpimath.controller import PIDController, ProfiledPIDController,SimpleMotorFeedforwardMeters, SimpleMotorFeedforwardRadians
-
+from wpimath.controller import PIDController, ProfiledPIDController, SimpleMotorFeedforwardMeters, SimpleMotorFeedforwardRadians
 from constants import ModuleConstants
 from rev import SparkMax, SparkMaxConfig, SparkBaseConfig, SparkBase
 from wpimath.geometry import Rotation2d
@@ -27,32 +19,16 @@ from wpimath.kinematics import SwerveModuleState, SwerveModulePosition
 from ntcore import NetworkTableInstance
 from commands2 import CommandScheduler
 
-
-
 class SwerveModule(commands2.SubsystemBase):
     def __init__(self, driveMotorChannel: int, turningMotorChannel: int, absEncoderChannel: int, absouteEncoderOffset: float) -> None:
-        """Constructs a SwerveModule with a drive motor, turning motor, drive encoder, and turning encoder. - JL
-
-        :param driveMotorChannel: PWM output for the drive motor.
-        :param turningMotorChannel: PWM output for the turning motor.
-        :param turningEncoderChannel: Analog input for the turning absolute encoder.
-        :param absoluteEncoderOffset: Offset for the absolute encoder.
-        """
+        """Constructs a SwerveModule with a drive motor, turning motor, drive encoder, and turning encoder."""
         super().__init__()
         # Force NetworkTables to start
         self.nt = NetworkTableInstance.getDefault()
-
-
-        # Create a NetworkTables entry for debugging
         self.debugTable = self.nt.getTable("SwerveDebug")
-
-        # Log NT startup
         print("NetworkTables started: SwerveDebug should be available")
 
-
         self.absouteEncoderOffset = 0
-
-
 
         self.driveMotor = SparkMax(driveMotorChannel, SparkMax.MotorType.kBrushless)
         self.turningMotor = SparkMax(turningMotorChannel, SparkMax.MotorType.kBrushless)
@@ -60,29 +36,22 @@ class SwerveModule(commands2.SubsystemBase):
         driveMotorConfig = SparkMaxConfig()
         turnMotorConfig = SparkMaxConfig()
 
-
         driveMotorConfig.encoder.positionConversionFactor(ModuleConstants.kdriveFactor)
-        driveMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kdriveFactor / 60.0) #! check this value
+        driveMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kdriveFactor / 60.0)  # check this value
 
         turnMotorConfig.encoder.positionConversionFactor(ModuleConstants.kturnFactor)
-        turnMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kturnFactor / 60.0) #! check this value
+        turnMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kturnFactor / 60.0)  # check this value
 
         self.driveMotor.configure(driveMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
         self.turningMotor.configure(turnMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
-
 
         # Setup encoders for the drive and turning motors.
         self.driveEncoder = self.driveMotor.getEncoder()
         self.turningEncoder = self.turningMotor.getEncoder()
 
-
         # Absolute Encoder
-        # Absolute Encoders help "remember" the location of the module.
-        # This is useful for when the robot is turned off and on again.
         self.absEncoder = AnalogEncoder(absEncoderChannel)
         self.absoluteEncoderOffsetRad = absouteEncoderOffset
-
-
 
         # Setup PID controllers for the driving and turning SPARKS MAX.
         self.drivePIDController = PIDController(ModuleConstants.kDriveP, ModuleConstants.kDriveI, ModuleConstants.kDriveD)
@@ -90,46 +59,40 @@ class SwerveModule(commands2.SubsystemBase):
         self.turningPIDController.enableContinuousInput(-math.pi, math.pi)
 
         self.driveFeedforward = SimpleMotorFeedforwardMeters(ModuleConstants.kDrivekS, ModuleConstants.kDrivekV)
-        self.turnFeedforward = SimpleMotorFeedforwardRadians(ModuleConstants.kTurningkS, ModuleConstants.kTurningkV)   #! check if FF radians is correct
+        self.turnFeedforward = SimpleMotorFeedforwardRadians(ModuleConstants.kTurningkS, ModuleConstants.kTurningkV)
 
-      
         # Set the idle mode for the motors to brake.
         driveMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
         turnMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
 
-
-
         self.chassisAngularOffset = absouteEncoderOffset
-
         self.resetEncoders()
 
-    
     def getAbsoluteEncoderRad(self) -> float:
         """Returns the absolute encoder position in radians minus the offset."""
         angle = self.absEncoder.get()
-        angle *= math.tau #? convert to radians
-        angle -= self.absoluteEncoderOffsetRad #? get acual location depending on the offset    
-        return angle 
-    
+        angle *= math.tau  # convert to radians
+        angle -= self.absoluteEncoderOffsetRad
+        return angle
+
     def getDrivePosition(self) -> float:
         """Returns the position of the drive encoder in meters."""
         return self.driveEncoder.getPosition()
-    
+
     def getTurningPosition(self) -> float:
         """Returns the position of the turning encoder in radians."""
         return self.turningEncoder.getPosition()
-    
+
     def getDriveVelocity(self) -> float:
         """Returns the velocity of the drive encoder in meters per second."""
         return self.driveEncoder.getVelocity()
-    
+
     def getTurningVelocity(self) -> float:
         """Returns the velocity of the turning encoder in radians per second."""
         return self.turningEncoder.getVelocity()
-    
 
     def resetEncoders(self):
-        """Resets the drive encoders to zero and Turn Encoders to value from absolute encoder in rads."""
+        """Resets the drive encoder to zero and turning encoder to the absolute encoder value."""
         self.driveEncoder.setPosition(0)
         self.turningEncoder.setPosition(self.getAbsoluteEncoderRad())
 
@@ -141,21 +104,35 @@ class SwerveModule(commands2.SubsystemBase):
         """Returns the current state of the module."""
         return SwerveModuleState(self.getDriveVelocity(), Rotation2d(self.getTurningPosition()))
 
-    def setDesiredState(self, state:SwerveModuleState):
+    def getModuleStates(self):
+        """Convenience method to return the state of this module (for use in drivetrain code)."""
+        return self.getState()
+    
+    
+    def optimizeState(self, desiredState: SwerveModuleState, currentAngle: Rotation2d) -> SwerveModuleState:
+        delta = desiredState.angle - currentAngle
+        if abs(delta.radians) > math.pi / 2:
+            return SwerveModuleState(-desiredState.speed, desiredState.angle + Rotation2d(math.pi))
+        return desiredState
+
+
+    def setDesiredState(self, state: SwerveModuleState):
         """Sets the desired state of the module."""
-        self.state = SwerveModuleState.optimize(state, self.getState().angle)
+        self.state = self.optimizeState(state, self.getState().angle)
 
         try:
             self.driveMotor.set(self.state.speed / ModuleConstants.kmaxModuleVelocity)
-        except:
+        except Exception as e:
+            print(f"Drive motor set error: {e}")
             self.driveMotor.set(0)
 
         try:
             self.turningMotor.set(self.turningPIDController.calculate(self.getAbsoluteEncoderRad(), self.state.angle.radians()))
-        except:
+        except Exception as e:
+            print(f"Turning motor set error: {e}")
             self.turningMotor.set(0)
-    
+
     def stop(self):
         """Stops the module."""
-        self.driveMotor.set(0)  
+        self.driveMotor.set(0)
         self.turningMotor.set(0)
