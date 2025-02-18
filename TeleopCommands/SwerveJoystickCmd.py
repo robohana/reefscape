cbeee4d945af1c7d52185a5e8d475d44060e84c7
Liliana <lilianacarlson.lrc@gmail.com>
2025-02-18 13:44:43 -0600

    Update SwerveJoystickCmd.py

--- TeleopCommands/SwerveJoystickCmd.py
+++ TeleopCommands/SwerveJoystickCmd.py
@@ -15,37 +15,34 @@ class SwerveJoystickCmd(Command):
         self.robotDrive = robotDrive
         self.driverController = driverController
         self.addRequirements(self.robotDrive)
-        #create Slew limiter
-        self.xLimiter = SlewRateLimiter(DrivingConstants.kTeleopDriveMaxAccelerationMetersPerSecSquared)
-        self.yLimiter = SlewRateLimiter(DrivingConstants.kTeleopDriveMaxAccelerationMetersPerSecSquared)
-        self.zRotLimiter = SlewRateLimiter(DrivingConstants.kTeleopDriveMaxAccelerationMetersPerSecSquared)
+        # create Slew limiter
+        self.xLimiter = SlewRateLimiter(DrivingConstants.kTeleDriveMaxAccelerationMetersPerSecSquared)
+        self.yLimiter = SlewRateLimiter(DrivingConstants.kTeleDriveMaxAccelerationMetersPerSecSquared)
+        self.zRotLimiter = SlewRateLimiter(DrivingConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond)
 
     def initialize(self):
         pass
     
     def execute(self):
-        #these are multiplied by the drivingSpeedLimiter which limit the speed of the robot so it doesn't go too fast
-        self.xSpeed = self.driverController.getLeftX() * DrivingConstants.drivingSpeedLimiter #self.drivingLimiter#* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
-        self.ySpeed = self.driverController.getLeftY() * DrivingConstants.drivingSpeedLimiter#self.drivingLimiter #* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
-        self.zRotation = self.driverController.getRightX() * -1 * DrivingConstants.rotationSpeedLimiter#self.drivingLimiter
+        # these are multiplied by the drivingSpeedLimiter which limit the speed of the robot so it doesn't go too fast
+        self.xSpeed = self.driverController.getLeftX() * DrivingConstants.drivingSpeedLimiter # self.drivingLimiter#* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
+        self.ySpeed = self.driverController.getLeftY() * DrivingConstants.drivingSpeedLimiter # self.drivingLimiter #* RobotConstants.kTeleopDriveMaxSpeedMetersPerSecond
+        self.zRotation = self.driverController.getRightX() * -1 * DrivingConstants.rotationSpeedLimiter # self.drivingLimiter
         
         # 1. Get the joystick values and apply deadzone
-        
-        
         self.xSpeed = wpimath.applyDeadband(self.xSpeed, OIConstants.deadzone)
         self.ySpeed = wpimath.applyDeadband(self.ySpeed, OIConstants.deadzone)
         self.zRotation = wpimath.applyDeadband(self.zRotation, OIConstants.deadzone)
         
 
         # # 2. Add rateLimiter to smooth the joystick values
-        self.xSpeed = self.xLimiter.calculate(self.xSpeed) * DrivingConstants.kMaxSpeedMetersPerSecond
-        self.ySpeed = self.yLimiter.calculate(self.ySpeed) * DrivingConstants.kMaxSpeedMetersPerSecond
-        self.zRotation = self.zRotLimiter.calculate(self.zRotation) * DrivingConstants.kMaxSpeedMetersPerSecond * DrivingConstants.kMaxSpeedMetersPerSecond / 0.418480
+        self.xSpeed = self.xLimiter.calculate(self.xSpeed) * DrivingConstants.kTeleDriveMaxSpeedMetersPerSecond
+        self.ySpeed = self.yLimiter.calculate(self.ySpeed) * DrivingConstants.kTeleDriveMaxSpeedMetersPerSecond
+        self.zRotation = self.zRotLimiter.calculate(self.zRotation) * DrivingConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond #DrivingConstants.kMaxSpeedMetersPerSecond * DrivingConstants.kMaxSpeedMetersPerSecond / 0.418480
         #! Not sure why this is needed, for some reason without it, the robot rotates slower 
 
-        # if self.feildOriented:
-        chasisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(self.xSpeed, self.ySpeed, 
-                                                            self.zRotation, self.robotDrive.getRotation2d())
+        # if self.fieldOriented:
+        chasisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(self.xSpeed, self.ySpeed, self.zRotation, self.robotDrive.getRotation2d())
         
         # else:
         # If robotOrinted is desired (But what's the fun in that?)
