348d24d4487dbdb8b7ce6882c1d0f1c00efded82
Jesse Lard <jesselard@gmail.com>
2025-02-18 08:55:16 -0600

    Update swervemodule.py

--- subsystems/swervemodule.py
+++ subsystems/swervemodule.py
@@ -6,14 +6,19 @@ import wpimath.kinematics
 import wpimath.geometry
 import wpimath.controller
 import wpimath.trajectory
-import wpimath.units
+from wpimath import units
 from wpimath.trajectory import TrajectoryConfig
 from wpilib import AnalogEncoder
 import commands2
 
 from wpimath.controller import PIDController, ProfiledPIDController, SimpleMotorFeedforwardMeters, SimpleMotorFeedforwardRadians
 from constants import ModuleConstants
-from rev import SparkMax, SparkMaxConfig, SparkBaseConfig, SparkBase
+
+from rev import SparkMax, SparkMaxConfig, SparkBase, SparkBaseConfig
+
+
+
+
 from wpimath.geometry import Rotation2d
 from wpimath.kinematics import SwerveModuleState, SwerveModulePosition
 from ntcore import NetworkTableInstance
@@ -35,11 +40,11 @@ class SwerveModule(commands2.SubsystemBase):
         driveMotorConfig = SparkMaxConfig()
         turnMotorConfig = SparkMaxConfig()
 
-        driveMotorConfig.encoder.positionConversionFactor(ModuleConstants.kdriveFactor)
-        driveMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kdriveFactor / 60.0)  # check this value
+        driveMotorConfig.encoder.positionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter)
+        driveMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec) 
 
-        turnMotorConfig.encoder.positionConversionFactor(ModuleConstants.kturnFactor)
-        turnMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kturnFactor / 60.0)  # check this value
+        turnMotorConfig.encoder.positionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad)
+        turnMotorConfig.encoder.velocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec)
 
         self.driveMotor.configure(driveMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
         self.turningMotor.configure(turnMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
@@ -53,7 +58,8 @@ class SwerveModule(commands2.SubsystemBase):
         self.absoluteEncoderOffsetRad = absouteEncoderOffset
 
         # Setup PID controllers for the driving and turning SPARKS MAX.
-        self.drivePIDController = PIDController(ModuleConstants.kDriveP, ModuleConstants.kDriveI, ModuleConstants.kDriveD)
+        self.drivePIDController = self.driveMotor.getClosedLoopController()
+        # self.drivePIDController = PIDController(ModuleConstants.kDriveP, ModuleConstants.kDriveI, ModuleConstants.kDriveD)
         self.turningPIDController = PIDController(ModuleConstants.kPTurning, ModuleConstants.kITurning, ModuleConstants.KDTurning)
         self.turningPIDController.enableContinuousInput(-math.pi, math.pi)
 
@@ -64,6 +70,7 @@ class SwerveModule(commands2.SubsystemBase):
         driveMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
         turnMotorConfig.setIdleMode(SparkBaseConfig.IdleMode.kBrake)
 
+        self.desiredState = SwerveModuleState()
         self.resetEncoders()
 
     def getAbsoluteEncoderRad(self) -> float:
@@ -71,7 +78,8 @@ class SwerveModule(commands2.SubsystemBase):
         raw = self.absEncoder.get()
 
         full_scale = 1.0172 #average full-scale value from testing
-        angle = (raw / full_scale) * math.tau #convert normalized value to radians
+        angle = units.rotationsToRadians(raw / full_scale) #convert normalized value to radians
+        # angle = (raw / full_scale) * math.tau #convert normalized value to radians
         angle -= self.absoluteEncoderOffsetRad
         return angle
 
@@ -102,37 +110,43 @@ class SwerveModule(commands2.SubsystemBase):
 
     def getState(self) -> SwerveModuleState:
         """Returns the current state of the module."""
-        return SwerveModuleState(self.getDriveVelocity(), Rotation2d(self.getTurningPosition()))
+        return SwerveModuleState(self.getDriveVelocity(), Rotation2d(self.getAbsoluteEncoderRad()))
 
     def getModuleStates(self):
         """Convenience method to return the state of this module (for use in drivetrain code)."""
         return self.getState()
     
     
-    def optimizeState(self, desiredState: SwerveModuleState, currentAngle: Rotation2d) -> SwerveModuleState:
-        delta = desiredState.angle - currentAngle
-        if abs(delta.radians()) > math.pi / 2:
-            return SwerveModuleState(-desiredState.speed, desiredState.angle + Rotation2d(math.pi))
-        return desiredState
+    # def optimizeState(self, desiredState: SwerveModuleState, currentAngle: Rotation2d) -> SwerveModuleState:
+    #     delta = desiredState.angle - currentAngle
+    #     if abs(delta.radians()) > math.pi / 2:
+    #         return SwerveModuleState(-desiredState.speed, desiredState.angle + Rotation2d(math.pi))
+    #     return desiredState
 
 
     def setDesiredState(self, state: SwerveModuleState):
-        """Sets the desired state of the module."""
-        self.state = self.optimizeState(state, self.getState().angle)
+        """Sets the desired state of the module, using PID and feedforward for the drive motor."""
+        # Optimize the state using our internal method.
+#        self.state = self.optimizeState(state, self.getState().angle)
+        # state.angle += self.absoluteEncoderOffsetRad
+        self.desiredState = state
+        state.optimize(self.getState().angle)
+        if math.isclose(state.speed, 0):
+            self.driveMotor.stopMotor()
+        else:
+            self.drivePIDController.setReference(state.speed, SparkBase.ControlType.kVelocity)
 
         try:
-            self.driveMotor.set(self.state.speed / ModuleConstants.kmaxModuleVelocity)
-        except Exception as e:
-            print(f"Drive motor set error: {e}")
-            self.driveMotor.set(0)
-
-        try:
-            self.turningMotor.set(self.turningPIDController.calculate(self.getAbsoluteEncoderRad(), self.state.angle.radians()))
+            # For the turning motor, we typically use PID control.
+            turningOutput = self.turningPIDController.calculate(self.getAbsoluteEncoderRad(), state.angle.radians())
+            self.turningMotor.set(turningOutput)
         except Exception as e:
             print(f"Turning motor set error: {e}")
             self.turningMotor.set(0)
 
+
     def stop(self):
         """Stops the module."""
-        self.driveMotor.set(0)
-        self.turningMotor.set(0)
+        self.driveMotor.stopMotor()
+        self.turningMotor.stopMotor()
+        
